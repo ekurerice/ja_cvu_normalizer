cbc70aeb6e4217dbc218ac21e58259cad8bf4f1c
ekurerice <alexandria.rindybell@gmail.com>
2021-08-11 14:28:46 +0900

    update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,35 @@
-from setuptools import setup
+import os
+import sys
+from setuptools import find_packages, setup
 
-setup()
+
+def decomment(file):
+    for row in file:
+        if "==" in row:
+            yield row
+        continue
+
+
+def read_requirements():
+    """Parse requirements from requirements.txt."""
+    reqs_path = os.path.join('.', 'requirements.txt')
+    with open(reqs_path, 'r') as f:
+        f = decomment(f)
+        requirements = [line.rstrip() for line in f]
+    print(requirements)
+    return requirements
+
+
+setup(
+    name="jp_cvu_normalizer",
+    version="0.2.0",
+    author="ekurerice",
+    url="https://github.com/ekurerice/jp_cvu_normalizer",
+    author_email="alexandria.rindybell@gmail.com",
+    packages=find_packages(exclude=["test"]),
+    package_data={
+    },
+    include_package_data=True,
+    install_requires=read_requirements(),
+    python_requires='>=3.6',
+)
